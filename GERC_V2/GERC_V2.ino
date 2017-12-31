3a50e79f0ba3da4b399c9a2b129a54108fcb9cab
travisrideout <travis.rideout518@gmail.com>
2017-12-31 11:25:07 -0500

    Add project files.

---
+++ GERC_V2/GERC_V2.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		GERC_V2.ino
+ Created:	12/31/2017 11:25:01 AM
+ Author:	Travi
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
